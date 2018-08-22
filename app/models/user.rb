bd7884aab5205fdc33036aceb70e0cebb8cd393c
bishosilwal <aayussilwal@gmail.com>
2018-08-22 16:35:02 +0545

    provider anduid saved after omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,6 @@ class User < ApplicationRecord
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
       user.token = auth.credentials.token
-     binding.pry
       
     end
   end
